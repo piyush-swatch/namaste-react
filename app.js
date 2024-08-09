387506cab41ea5562832246e42668bc7969f72c8
piyush-swatch <piyush.swatch@next-ventures.com>
2024-08-09 17:51:09 +0530

    ep3

--- app.js
+++ app.js
@@ -1,21 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-const nestedElement = React.createElement('div', {'id': 'parent'},
-    [
-        React.createElement('div', {'id': 'child'}, 
-        [React.createElement('h1', {'id': 'heading'}, 'This is Namaste React'),
-        React.createElement('h2', {'id': 'heading2'}, 'I am a heading 2')] 
-        ),
-        React.createElement('div', {'id': 'child2'}, 
-        [React.createElement('h1', {'id': 'heading'}, 'I am a heading'),
-        React.createElement('h2', {'id': 'heading2'}, 'I am a heading 2')] 
-        )  
-    ]  
-);
+// React Element without JSX
+const heading = React.createElement("h1", { 'id': 'hello' }, 'Heading without JSX');
 
-console.log(nestedElement);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+// React Element using JSX - not pure JS, hence transpiled by Parcel using Babel package
+// Babel transpile/converts jsx to React object (the same object being created by React.createElement)
+const jsxHeading = <h1 id="hello" className="jsxClass">Heading using JSX</h1>;
 
-root.render(nestedElement);
+// multiple lines JSX syntax - wrap inside paranthesis
+const JsxHeading2 = () => {
+    return (
+        <h1
+            id="hello2"
+            className="jsxClass2">
+            Heading using JSX multiple lines...
+        </h1>)
+}
+
+const teacherName = 'Akshay Saini';
+
+// React Functional Components - basically function returning the JSX Element
+const HeadingComponent = () => {
+    return (
+        <div className="container">
+
+            {jsxHeading}
+
+            {/* using component composition */}
+            <JsxHeading2 />
+            {JsxHeading2()}
+            <JsxHeading2 ></JsxHeading2>
+
+            {/* Using javascript statement inside JSX */}
+            by {teacherName}
+
+            <h1 id="hello">
+                Namaste React functional component
+            </h1>
+        </div>
+    )
+}
+
+
+// get root element from HTML
+const rootElement = ReactDOM.createRoot(document.getElementById('root'));
+
+// render
+rootElement.render(<HeadingComponent />);
